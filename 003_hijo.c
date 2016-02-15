0851244f86c82722f97be7187d2bfdfe3f5296a7
luisoran <luisoran@gmail.com>
2016-02-15 03:55:12 +0100

    Proceso hijo para pruebas.

---
+++ 003_hijo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main (int argc, char *argv[])
+{
+	//sleep(30);
+	printf("proceso hijo con pid %ld con %s hermanos\n", (long)getpid(), argv[1]);
+
+	return 0;
+}
